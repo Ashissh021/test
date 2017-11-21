bcad638b65c3c5da979b006fd5422a6acd77b2f3
Ashissh021 <32160490+Ashissh021@users.noreply.github.com>
2017-11-21 22:37:29 +0530

    junkdaata

---
+++ rowdata.java
@@ -0,0 +1,31 @@
+{"QueryResult": 
+	{
+		"TotalResultCount": 1236,
+		"StartIndex": 1,
+		"PageSize": 20,
+		"Results": [{"_ref": "/portfolioitem/feature/43648905776","_refObjectUUID": "11149fe7-b207-4a4e-b1c8-93c9ee25cdce","_refObjectName": "Nov'15 Mock","Name": "Nov'15 Mock","_type": "PortfolioItem/Feature"},
+				{"_ref": "/portfolioitem/feature/45404372666", "_refObjectUUID": "cd65534f-5d9b-470e-8178-1310aec6f3b1", "_refObjectName": "WC - Unscheduled Enhancements", "Name": "WC - Unscheduled Enhancements", "_type": "PortfolioItem/Feature"},
+				{"_ref": "/portfolioitem/feature/47083639309", "_refObjectUUID": "b6ae7793-0c96-49cd-b533-b9cdf8ad271b", "_refObjectName": "August - Production Environment", "Name": "August - Production Environment", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48011307293", "_refObjectUUID": "7b6e5f82-66ad-44f1-af52-73ca294335c2", "_refObjectName": "Actionable Outcomes", "Name": "Actionable Outcomes", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48011880840", "_refObjectUUID": "6aba4759-834c-4ff0-bf9a-e09e22d204be", "_refObjectName": "Process For the Sake of Process", "Name": "Process For the Sake of Process", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48011887804", "_refObjectUUID": "97376126-e746-42cf-9acc-47b1183d9d49", "_refObjectName": "Waterfall WDD", "Name": "Waterfall WDD", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48011888304", "_refObjectUUID": "0fcd3099-68f1-4fa3-b35e-3f73e4218c9e", "_refObjectName": "Agile WDD", "Name": "Agile WDD", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48011888614", "_refObjectUUID": "87544dbc-92df-4713-9150-0befa1eb2775", "_refObjectName": "Prod Spt WDD", "Name": "Prod Spt WDD", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48011889313", "_refObjectUUID": "135097d9-a038-4a82-8fd2-633a0f6627df", "_refObjectName": "Work Order Updates", "Name": "Work Order Updates", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48012415321", "_refObjectUUID": "736a7983-dd27-48d0-900b-73cb4fd6546e", "_refObjectName": "Redefine Testing by Application Components", "Name": "Redefine Testing by Application Components", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48040078309", "_refObjectUUID": "6cd322ca-9000-47a2-8095-8ddcd2c11a3a", "_refObjectName": "Quality Review", "Name": "Quality Review", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48044987063", "_refObjectUUID": "8602ffb2-e9b6-484b-8680-998dec161aab", "_refObjectName": "3rd Party Vendor Testing Methodology", "Name": "3rd Party Vendor Testing Methodology", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48044989419", "_refObjectUUID": "030aa535-751a-4f18-8f3e-a65ca3bcd608", "_refObjectName": "QE Role Clarity", "Name": "QE Role Clarity", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48051301849", "_refObjectUUID": "47dee0a5-8c2e-4217-a35b-3ead3fbc186b", "_refObjectName": "Data Quality", "Name": "Data Quality", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48051875326", "_refObjectUUID": "e46f93f7-592a-40c5-bffa-f887b6cd1727", "_refObjectName": "Availability of Functionality", "Name": "Availability of Functionality", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48051957295", "_refObjectUUID": "23ba8773-05cb-44dd-a678-f32423da1ec4", "_refObjectName": "Data Ownership", "Name": "Data Ownership", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48052670984", "_refObjectUUID": "7578a4d1-49ca-4e5f-a69c-9c6782381432", "_refObjectName": "Cross Lines Testing Methodology", "Name": "Cross Lines Testing Methodology", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48052775818", "_refObjectUUID": "b894b6cd-e944-41fa-8d3a-bc9e1b4d93c2", "_refObjectName": "Clarification Process", "Name": "Clarification Process", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48052887207", "_refObjectUUID": "2c8d3faf-b116-4466-b6d2-60abc8292102", "_refObjectName": "Risk Based Testing Process & Criteria", "Name": "Risk Based Testing Process & Criteria", "_type": "PortfolioItem/Feature"}, 
+				{"_ref": "/portfolioitem/feature/48060533414", "_refObjectUUID": "3b6a0e0f-1a36-4930-a901-def7eaeeb79f", "_refObjectName": "Impact Analysis Template", "Name": "Impact Analysis Template", "_type": "PortfolioItem/Feature"}
+				]
+	}
+}
+{"QueryResult": {"TotalResultCount": 1236,"StartIndex": 1,"PageSize": 20,
+    "Results": [{"_ref": "/portfoliosimFeatureb/something/value","_refObjectId": "1259974-tr155jht13","_refName": "WC Unsche3duled-INC","Name": "August Productin","_type": "/PortfoleoItem/Feature"},
+      {"_ref": "/portfoliosimFeatureb/something/value","_refObjectId": "1259974-tr155jht13","_refName": "WC Unsche3duled-INC","Name": "August Productin","_type": "/PortfoleoItem/Feature"}]}}
